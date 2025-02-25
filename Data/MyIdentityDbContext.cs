97d64b85205ab160580af80e6eeaf3fb9c8bedf6
Wad-Laptop <iamwad@gmail.com>
2025-02-25 21:00:19 +0700

    Add DbContext for IdentityDbContext named MyIdentityDbContext

---
+++ Data/MyIdentityDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DotnetCore8API_Identity.Data
+{
+    public class MyIdentityDbContext : IdentityDbContext
+    {
+
+        public MyIdentityDbContext(DbContextOptions<MyIdentityDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
