1e01c68e0e9aa8859c567c47ff704f254ab71bf2
Wad-Laptop <iamwad@gmail.com>
2025-02-25 22:14:52 +0700

    Add Authorize anotation to WeatherForecastController that make it need to append JWT Token on header for access this controller.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotnetCore8API_Identity.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
